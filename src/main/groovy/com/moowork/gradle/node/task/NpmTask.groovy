a6535c239715f044ec0a1c94bf62c3f1e8779429
John Engelman <john.r.engelman@gmail.com>
2014-04-15 10:24:45 -0500

    store the exec result on the task

--- src/main/groovy/com/moowork/gradle/node/task/NpmTask.groovy
+++ src/main/groovy/com/moowork/gradle/node/task/NpmTask.groovy
@@ -3,6 +3,7 @@ package com.moowork.gradle.node.task
 import com.moowork.gradle.node.exec.NpmExecRunner
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecResult
 
 class NpmTask
     extends DefaultTask
@@ -10,6 +11,8 @@ class NpmTask
     private NpmExecRunner runner
 
     private Iterable<?> args = []
+
+    private ExecResult result
 
     def String[] npmCommand
 
@@ -44,6 +47,20 @@ class NpmTask
         this.runner.workingDir = value
     }
 
+    void setIgnoreExitValue ( final boolean value )
+    {
+        this.runner.ignoreExitValue = value
+    }
+
+    void setExecOverrides ( final Closure closure )
+    {
+        this.runner.execOverrides = closure
+    }
+
+    ExecResult getResult() {
+        return this.result
+    }
+
     @TaskAction
     void exec()
     {
@@ -51,6 +68,6 @@ class NpmTask
             this.runner.arguments.addAll(this.npmCommand)
         }
         this.runner.arguments.addAll(this.args)
-        this.runner.execute()
+        this.result = this.runner.execute()
     }
 }
