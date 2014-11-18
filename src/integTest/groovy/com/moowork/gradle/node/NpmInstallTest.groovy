8d532b50ea68c35faf820a8a425a73e9ccf4e703
Sten Roger Sandvik <srs@enonic.com>
2014-11-18 22:50:50 +0100

    Added simple integration test using nebula framework. Not enabled by default.

---
+++ src/integTest/groovy/com/moowork/gradle/node/NpmInstallTest.groovy
@@ -0,0 +1,44 @@
+package com.moowork.gradle.node
+
+import nebula.test.IntegrationSpec
+
+class NpmInstallTest
+    extends IntegrationSpec
+{
+    def 'install packages with npm'()
+    {
+        when:
+        writeEmptyPackageJson()
+        this.buildFile << applyPlugin( NodePlugin )
+        this.buildFile << '''
+            node {
+                version = "0.10.33"
+                npmVersion = "2.1.6"
+                download = true
+                workDir = file('build/node')
+            }
+        '''.stripIndent()
+
+        def result = runTasksSuccessfully( 'npmInstall' )
+
+        then:
+        !result.wasUpToDate( 'npmInstall' )
+        fileExists( 'node_modules' )
+
+        when:
+        result = runTasksSuccessfully( 'npmInstall' )
+
+        then:
+        result.wasUpToDate( 'npmInstall' )
+    }
+
+    def writeEmptyPackageJson()
+    {
+        def packageJson = createFile( 'package.json', this.projectDir )
+        packageJson << """{
+            "name": "example",
+            "dependencies": {
+            }
+        }""".stripIndent()
+    }
+}
