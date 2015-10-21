136934ea0de5892d9181a48f01ec441de99febc5
Sten Roger Sandvik <srs@enonic.com>
2015-10-21 23:33:44 +0200

    Added more windows tests

--- src/integTest/groovy/com/moowork/gradle/node/Setup_integTest.groovy
+++ src/integTest/groovy/com/moowork/gradle/node/Setup_integTest.groovy
@@ -5,6 +5,13 @@ import com.moowork.gradle.AbstractIntegTest
 class Setup_integTest
     extends AbstractIntegTest
 {
+    private final static OS_NAME = System.getProperty( 'os.name' )
+
+    def cleanup()
+    {
+        System.setProperty( 'os.name', OS_NAME )
+    }
+
     def 'setup node'()
     {
         given:
@@ -26,6 +33,48 @@ class Setup_integTest
 
     def 'setup node (download)'()
     {
+        given:
+        writeBuild( '''
+            apply plugin: 'com.moowork.node'
+
+            node {
+                version = "0.10.33"
+                download = true
+            }
+        ''' )
+
+        when:
+        def result = runTasksSuccessfully( 'nodeSetup' )
+
+        then:
+        result.wasExecuted( 'nodeSetup' )
+    }
+
+    def 'setup node (windows)'()
+    {
+        System.setProperty( 'os.name', 'Windows' )
+
+        given:
+        writeBuild( '''
+            apply plugin: 'com.moowork.node'
+
+            node {
+                version = "0.10.33"
+                download = false
+            }
+        ''' )
+
+        when:
+        def result = runTasksSuccessfully( 'nodeSetup' )
+
+        then:
+        result.wasExecuted( 'nodeSetup' )
+    }
+
+    def 'setup node (windows download)'()
+    {
+        System.setProperty( 'os.name', 'Windows' )
+
         given:
         writeBuild( '''
             apply plugin: 'com.moowork.node'
