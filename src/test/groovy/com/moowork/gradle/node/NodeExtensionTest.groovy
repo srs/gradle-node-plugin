1a224ccca2be1d9bb69d8e68c11339f91971bc77
Brett Epps <brett.epps@software.dell.com>
2016-07-07 18:55:32 -0500

    Implement credentials support for distBaseUrl (closes #126)

--- src/test/groovy/com/moowork/gradle/node/NodeExtensionTest.groovy
+++ src/test/groovy/com/moowork/gradle/node/NodeExtensionTest.groovy
@@ -1,5 +1,7 @@
 package com.moowork.gradle.node
 
+import org.gradle.api.artifacts.repositories.PasswordCredentials
+
 class NodeExtensionTest
     extends AbstractProjectTest
 {
@@ -13,6 +15,8 @@ class NodeExtensionTest
         ext != null
         ext.npmCommand == 'npm'
         ext.distBaseUrl == 'https://nodejs.org/dist'
+        ext.distCredentialsType == PasswordCredentials
+        ext.distCredentialsAction == null
         ext.workDir != null
         ext.nodeModulesDir != null
         ext.version == '4.4.0'
