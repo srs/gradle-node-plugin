03a077770bb6824fbd5c0caf6d231263d3968004
Ivo Senner <ivo.senner@googlemail.com>
2016-12-19 10:40:21 +0100

    Test if postinstall tasks work

--- src/test/groovy/com/moowork/gradle/node/yarn/YarnInstall_integTest.groovy
+++ src/test/groovy/com/moowork/gradle/node/yarn/YarnInstall_integTest.groovy
@@ -37,6 +37,42 @@ class YarnInstall_integTest
         result.outcome == TaskOutcome.UP_TO_DATE
     }
 
+    def 'install packages with yarn and and postinstall task requiring node and yarn'()
+    {
+        given:
+        writeBuild( '''
+            plugins {
+                id 'com.moowork.node'
+            }
+            node {
+                version = "6.9.1"
+                yarnVersion = "0.16.1"
+                download = true
+                workDir = file('build/node')
+                yarnWorkDir = file('build/yarn')
+            }
+        ''' )
+        writePackageJson(""" {
+            "name": "example",
+            "dependencies": {},
+            "versionOutput" : "node --version",
+            "postinstall" : "yarn run versionOutput"
+        }
+        """)
+
+        when:
+        def result = buildTask( 'yarn' )
+
+        then:
+        result.outcome == TaskOutcome.SUCCESS
+
+        when:
+        result = buildTask( 'yarn' )
+
+        then:
+        result.outcome == TaskOutcome.UP_TO_DATE
+    }
+
     def 'install packages with yarn in different directory'()
     {
         given:
