52aee25f9dda576fbf59640f3106cfd0f77b5fcb
Mark Vieira <portugee@gmail.com>
2017-03-20 09:54:35 -0700

    Emulate non-annotation style input mapping.
    This effectively disabled the validation done by using input annotations.
    The result is the same behavior as using `TaskInputs.files` which is to
    not validate that the input files are present and exist. While not preferable
    this at least limits breaking existing users which might have a unique
    configuration.

--- src/main/groovy/com/moowork/gradle/node/yarn/YarnInstallTask.groovy
+++ src/main/groovy/com/moowork/gradle/node/yarn/YarnInstallTask.groovy
@@ -2,6 +2,7 @@ package com.moowork.gradle.node.yarn
 
 import com.moowork.gradle.node.NodeExtension
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
@@ -19,20 +20,30 @@ class YarnInstallTask
         this.description = 'Install node packages using Yarn.'
         setYarnCommand( '' )
         dependsOn( [YarnSetupTask.NAME] )
+
+        this.project.afterEvaluate {
+            getInputs().file( new File( (File) this.project.node.nodeModulesDir, 'package.json' ) )
+            getInputs().file( new File( (File) this.project.node.nodeModulesDir, 'yarn.lock' ) )
+            getOutputs().dir( new File( (File) this.project.node.nodeModulesDir, 'node_modules' ) )
+        }
     }
 
     @InputFile
+    @Optional
     @PathSensitive(PathSensitivity.RELATIVE)
     protected getPackageJsonFile()
     {
-        return new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'package.json')
+        def packageJsonFile = new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'package.json')
+        return packageJsonFile.exists() ? packageJsonFile : null
     }
 
     @InputFile
+    @Optional
     @PathSensitive(PathSensitivity.RELATIVE)
     protected getYarnLockFile()
     {
-        return new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'yarn.lock')
+        def lockFile = new File(this.project.extensions.getByType(NodeExtension).nodeModulesDir, 'yarn.lock')
+        return lockFile.exists() ? lockFile : null
     }
 
     @OutputDirectory
